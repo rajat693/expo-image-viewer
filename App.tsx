9c3f7e971608b4809631c4c98e247f288bd68fac
rajat693 <rajatchaudhary.rc693@gmail.com>
2024-12-19 23:02:14 +0530

    feat: added gluestack components

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { Text, View } from "react-native";
+import { SafeAreaView } from "@/components/ui/safe-area-view";
+import { Text } from "./components/ui/text";
 
 export default function App() {
   return (
     <GluestackUIProvider mode="light">
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-red-600">
+      <SafeAreaView className="flex-1 justify-center items-center">
+        <Text className="text-red-600" size="xl">
           Open up App.tsx to start working on your app!
         </Text>
-      </View>
+      </SafeAreaView>
     </GluestackUIProvider>
   );
 }
